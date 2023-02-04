d06c315ad92c5de3f24ee92c20477a9a122aa2db
ornao <reynoldsorna@gmail.com>
2023-02-04 15:45:01 +0000

    add form validation to username input

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,15 @@
-document.getElementById("startGame").onclick = function () {
-        location.href = "level.html";
-    };
+const form = document.getElementById("form");
+form.addEventListener('submit', validateForm);
 
-    
+function validateForm(event) {
+    event.preventDefault();
+
+    const username = event.target.username.value;
+
+    if (username.length > 3) {
+            location.href = "level.html";
+        }
+    else alert ('Please enter a username with at least 4 characters')
+}
+
+
