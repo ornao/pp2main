9ce0935e101556747c0a5c33a5fb34e57991c143
ornao <reynoldsorna@gmail.com>
2023-02-04 12:47:00 +0000

    add game page, map element and links

---
+++ assets/js/level.js
@@ -0,0 +1,11 @@
+document.getElementById("easy").onclick = function () {
+    location.href = "game.html";
+};
+
+document.getElementById("medium").onclick = function () {
+    location.href = "game.html";
+};
+
+document.getElementById("hard").onclick = function () {
+    location.href = "game.html";
+};
