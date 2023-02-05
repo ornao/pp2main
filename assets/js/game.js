f4f48f43cbca574b8152f61d004f6435b7455228
ornao <reynoldsorna@gmail.com>
2023-02-05 16:08:04 +0000

    add country names to.json and load to console

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,24 @@
 const userName = localStorage.getItem('user-name');
 document.getElementById('user-name').textContent = userName;
 
-// let countries = [];
+let countries = [];
 
-// fetch(
-//     'https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple'
-// )
-//     .then(res => {
-//         return res.json();
-//     })
-//     .then(loadedCountries => {
-//         console.log(loadedCountries);
-//         countries = loadedCountries;
-//         startGame();
-//     })
-//     .catch(err => {
-//         console.error(err);
-//     });
+fetch("countries.json")
+    .then(res => {
+        return res.json();
+    })
+    .then(loadedCountries => {
+        console.log(loadedCountries);
+        countries = loadedCountries;
+        startGame();
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+    startGame = () => {
+        questionCounter = 0;
+        score = 0;
+        availableCountries = [...countries];
+    };
            
