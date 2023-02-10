e82572ee5fb7ee264c8666c82e62dfbb05162d09
ornao <reynoldsorna@gmail.com>
2023-02-10 19:18:23 +0000

    add comments to level.js to make code clear

--- assets/js/level.js
+++ assets/js/level.js
@@ -1,17 +1,18 @@
+// get user input from index.html and display it as user-name 
 const userName = localStorage.getItem('user-name');
 document.getElementById('user-name').textContent = userName;
 
-
+// when easy button clicked, load what 'easy" to local storage so have access to in next page
 document.getElementById("easy").onclick = function levelEasy () {
     location.href = "game.html";
     localStorage.setItem('game-difficulty', 'easy');
 };
-
+// when medium button clicked, load what 'medium" to local storage so have access to in next page
 document.getElementById("medium").onclick = function levelMedium () {
     location.href = "game.html";
     localStorage.setItem('game-difficulty', 'medium');
 };
-
+// when hard button clicked, load what 'hard" to local storage so have access to in next page
 document.getElementById("hard").onclick = function levelHard () {
     location.href = "game.html";
     localStorage.setItem('game-difficulty', 'hard');
