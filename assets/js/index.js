45ab9c5a33dcea530329aa8bb12f8d97d422ad93
ornao <reynoldsorna@gmail.com>
2023-05-03 18:03:37 +0000

    bug: remove function calls in index.js of blur, focus

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,27 +5,24 @@
     input.style.width = "12rem";
     input.placeholder = "    Don't forget your username!";
   }
-// fucntion that removes color and text once clciked again
+// fucntion that removes color and text once clicked again
   function focusFunction() {
     let input = document.getElementById("username");
     document.getElementById("username").style.background = "#f5fbff";
     input.placeholder = "";
   }
 
-  blurFunction();
-  focusFunction();
+// once submit clicked validate user input
+// if right save to local storage and go to next page
+// if wrong give alert
+const form = document.getElementById("form"); form.addEventListener('submit', validateForm);
 
-        // once submit clicked validate user input
-        // if right save to local storage and go to next page
-        // if wrong give alert
-        const form = document.getElementById("form"); form.addEventListener('submit', validateForm);
+  function validateForm(event) {
+    event.preventDefault();
 
-        function validateForm(event) {
-            event.preventDefault();
-
-            const username = event.target.username.value;
-            if (username.length > 3) {
-                localStorage.setItem('user-name', username);
-                location.href = "level.html";
-            } else alert('Please enter a username with at least 4 characters');
-        }
+    const username = event.target.username.value;
+      if (username.length > 3) {
+        localStorage.setItem('user-name', username);
+        location.href = "level.html";
+      } else alert('Please enter a username with at least 4 characters'); 
+  }
